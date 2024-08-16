3fe42e83552d2a9dc0f70bc881d1b2a02e763549
tiwarisiddhi <fromtiwarisiddhi@gmail.com>
2024-08-16 08:42:14 +0530

    array programms

---
+++ array.js
@@ -0,0 +1,10 @@
+//Calculate the sum of all the numbers in the following array
+//eg - [1,13,22,123,49]
+let sum = 0;
+let array = [1,13,22,123,49];
+for(let a = 0; a<=array.length-1; a++){
+    sum = sum + array[a];
+    
+
+} 
+console.log(sum);  

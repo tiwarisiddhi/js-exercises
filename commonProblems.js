77024458d7628dd4fbf993322aacc0b6f89c2eb6
tiwarisiddhi <fromtiwarisiddhi@gmail.com>
2024-08-17 10:48:05 +0530

    reverse a string

--- commonProblems.js
+++ commonProblems.js
@@ -1,9 +1,11 @@
- //Write a JavaScript function that reverses a number.
+ //Write a JavaScript function that reverses a string.
  let x = '123';
+  let reverse = '';
  for(let a = x.length-1; a >= 0  ; a--){
-    let reverse = ''
-    reverse = '' + a
-    console.log(x[a]);
+    reverse = reverse + x[a]
+    
  }
-
+ 
+     console.log(reverse)
+ 
  

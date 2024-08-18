9ffa3da31424bebf39446804421d9a906b357655
tiwarisiddhi <fromtiwarisiddhi@gmail.com>
2024-08-18 10:37:20 +0530

    js function

---
+++ function.js
@@ -0,0 +1,57 @@
+function name(a){
+    return 6 + a
+
+}
+  
+
+let i = 10;
+ let total = name(i);
+ name(10);
+
+ console.log(total);
+ 
+ total = name(total);
+ console.log(total);
+ total = total + 6;
+ console.log(total);
+ 
+ function swap(l , p){
+    let dub = l;
+    l = p;
+    p = dub;
+    console.log(l , p);
+    
+}
+
+ 
+
+let a = 10;
+let b = 15;
+swap(a , b);
+let x = 20;
+let y = 25;
+
+swap(x , y);
+
+
+
+let array = [1 , 10, 15, 2, 3];
+console.log(array);
+array.push(6);
+console.log(array);
+array.push(7);
+console.log(array);
+array.pop();
+console.log(array);
+array.reverse();
+console.log(array);
+array.toString();
+console.log(array.toString());
+
+
+ 
+ 
+ 
+  
+
+

a400d05350c0d03f5c309e1e9a027f059a128b82
tiwarisiddhi <fromtiwarisiddhi@gmail.com>
2024-08-21 08:46:12 +0530

    practice

--- function.js
+++ function.js
@@ -1,57 +1,89 @@
-function name(a){
-    return 6 + a
+// function name(a){
+//     return 6 + a
 
-}
+// }
   
 
-let i = 10;
- let total = name(i);
- name(10);
+// let i = 10;
+//  let total = name(i);
+//  name(10);
 
- console.log(total);
+//  console.log(total);
  
- total = name(total);
- console.log(total);
- total = total + 6;
- console.log(total);
+//  total = name(total);
+//  console.log(total);
+//  total = total + 6;
+//  console.log(total);
  
- function swap(l , p){
-    let dub = l;
-    l = p;
-    p = dub;
-    console.log(l , p);
+//  function swap(l , p){
+//     let dub = l;
+//     l = p;
+//     p = dub;
+//     console.log(l , p);
     
+// }
+
+ 
+
+// let a = 10;
+// let b = 15;
+// swap(a , b);
+// let x = 20;
+// let y = 25;
+
+// swap(x , y);
+
+
+
+// let array = [1 , 10, 15, 2, 3];
+// console.log(array);
+// array.push(6);
+// console.log(array);
+// array.push(7);
+// console.log(array);
+// array.pop();
+// console.log(array);
+// array.reverse();
+// console.log(array);
+// array.toString();
+// console.log(array.toString());
+
+
+
+// anonymous function:-
+let test = function(msg){
+    console.log(msg);
+}
+ 
+ 
+ test(6);
+ 
+//  named function:-
+ function s(a){
+    console.log(a)
+ }
+  
+s(5);
+
+
+// arrow function:-
+let arrow = () => {
+    console.log('siddhi');
 }
 
- 
+arrow();
 
-let a = 10;
-let b = 15;
-swap(a , b);
-let x = 20;
-let y = 25;
-
-swap(x , y);
-
-
-
-let array = [1 , 10, 15, 2, 3];
-console.log(array);
-array.push(6);
-console.log(array);
-array.push(7);
-console.log(array);
-array.pop();
-console.log(array);
-array.reverse();
-console.log(array);
-array.toString();
-console.log(array.toString());
+// // higher order function:-
+// function hello(){
+// console.log('h');
+// return function newhello(msg){
+//     console.log(msg);
+// }
+// }
 
 
- 
- 
- 
-  
+// let a = hello();
 
+// a('siddhi');
 
+// function w()

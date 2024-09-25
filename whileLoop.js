89ebbb9018b044061d88ab9461bbd0ab1527c563
tiwarisiddhi <fromtiwarisiddhi@gmail.com>
2024-09-25 09:51:14 +0530

    whilelopp

--- whileLoop.js
+++ whileLoop.js
@@ -1,25 +1,82 @@
-let i = 1;
-while(i<21){
-    console.log(i);
-    i++;
+// let i = 1;
+// while(i<21){
+//     console.log(i);
+//     i++;
+// }
+
+// for(let i = 1; i < 21; i++){
+//     console.log(i);
+// }
+
+
+// let x = 123;
+// let reverseNumber = 0;
+// while(x !== 0){
+//    reverseNumber = reverseNumber*10 + x % 10;
+//    x = Math.floor(x/10);
+  
+// }
+// console.log(reverseNumber)
+
+
+// Write a while loop that prints numbers from 1 to 10.
+let a = 1;
+while(a < 11){
+    console.log(a);
+    a++
+    
+}
+// Write a while loop that sums the numbers from 1 to 100 and prints the total.
+let b = 1;
+let c = 0;
+while(b <= 100){
+    c = b + c;
+    b++
 }
 
-for(let i = 1; i < 21; i++){
-    console.log(i);
+console.log(c);
+
+
+    // Write a while loop to calculate the factorial of a given number n. 
+    // For example, if n = 5, the result should be 5 * 4 * 3 * 2 * 1 = 120
+
+let n = 5;
+let e = 1;
+while(n > 0){
+    e = n*e
+    n--
 }
+console.log(e);
 
-
-let x = 123;
-let reverseNumber = 0;
-while(x !== 0){
-   reverseNumber = reverseNumber*10 + x % 10;
-   x = Math.floor(x/10);
+    // Write a while loop that takes a string and prints it in reverse order. 
+    // For example, if the input is "hello", the output should be "olleh".
+let w = 'hello';
+let p = w.length-1;
+let v = '';
+while(p >= 0 ){
+    
+    v = v + w[p];
+    p--
   
 }
-console.log(reverseNumber)
+
+console.log(v);
+
+
+//    Write a while loop to count the number of digits in a given number. 
+//    For example, for the number 12345, the loop should return 5.
 
 
 
 
 
+
+// Given an array of numbers, write a while loop to find and print the largest number in the array.
+
+
+
+
+
+
+// Write a while loop that prints numbers from 1 to 100, but only if the number is divisible by both 3 and 5.
 
